26f33d83be55453fcc9d366ef72b9f9830ac6405
zLukas <lukasz.grab@intel.com>
2023-04-27 13:14:58 +0200

    multi value regions flag added

--- pkg/input/input.go
+++ pkg/input/input.go
@@ -1,18 +1,24 @@
 package input
 
-import "flag"
+import (
+	"flag"
+	"strings"
+)
 
 type Input struct {
 	KeyName          *string
 	KeyType          *string
 	AwsSharedProfile *string
-	AwsRegion        *string
+	AwsRegions       []string
+	ConfigFile       *string
 }
 
 func (i *Input) ParseArgs() {
+
 	i.KeyName = flag.String("n", "my-key", "keyname, default my-key")
 	i.KeyType = flag.String("t", "rsa", "type of encryption, default rsa")
 	i.AwsSharedProfile = flag.String("p", "", "(optional)aws shared profile name")
-	i.AwsRegion = flag.String("rg", "None", "region to log in, optional if specified in shared profile")
+	awsRegions := flag.String("rg", "None", "region to log in, optional if specified in shared profile")
 	flag.Parse()
+	i.AwsRegions = strings.Split(*awsRegions, ",")
 }
